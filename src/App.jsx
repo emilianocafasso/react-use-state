5003d817d3a43c2443519a38eb8394c272f67510
emilianocafasso <emiliano2.cafasso2@gmail.com>
2025-11-11 19:20:54 +0100

    add selected language description onClick & some styling

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,31 @@ export default function App() {
   const [selectedLanguage, setlectedLanguage] = useState(languages[0])
 
   return (
-    <div className='container mt-4'>
-      <h1>Learn Web development</h1>
+    <div>
+      <h1 className='d-flex bg-light pb-2'>Learn Web development</h1>
 
       {/* lista bottoni */}
+      <div className="container mt-4">
+        <div className="d-flex flex-wrap gap-2 mb-4">
+          {languages.map(language => (
+            <button
+              key={language.id}
+              className='btn btn-primary'
+              onClick={() => setlectedLanguage(language)}
+            >
+              {language.title}
+            </button>
+          ))}
+        </div>
 
-      <div className="d-flex flex-wrap gap-2 mb-4">
-        {languages.map(language => (
-          <button
-            key={language.id}
-            className='btn btn-primary'
-            onClick={() => setlectedLanguage(language)}
-          >
-            {language.title}
-          </button>
-        ))}
+        {/* card linguaggio selezionato */}
+        <div className='card'>
+          <div className="card-body">
+            <h5 className='card-title'>{selectedLanguage.title}</h5>
+            <p className="card-text">{selectedLanguage.description}</p>
+          </div>
+        </div>
       </div>
-
     </div>
   )
 }
