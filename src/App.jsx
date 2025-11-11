14d7fe410555fb0c8153506567339cc47ca8b028
emilianocafasso <emiliano2.cafasso2@gmail.com>
2025-11-11 19:14:48 +0100

    add button list

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import React, { useState } from 'react'
-import './App.css'
 import languages from './assets/use_State_assets/languages'
+import './App.css'
 
 export default function App() {
 
-  return (
-    <>
+  const [selectedLanguage, setlectedLanguage] = useState(languages[0])
 
-    </>
+  return (
+    <div className='container mt-4'>
+      <h1>Learn Web development</h1>
+
+      {/* lista bottoni */}
+
+      <div className="d-flex flex-wrap gap-2 mb-4">
+        {languages.map(language => (
+          <button
+            key={language.id}
+            className='btn btn-primary'
+            onClick={() => setlectedLanguage(language)}
+          >
+            {language.title}
+          </button>
+        ))}
+      </div>
+
+    </div>
   )
 }
